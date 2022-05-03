2deddea6fbad8b6d70ae3a7e59e3c99ceb6ff70d
Shun Usami <susami@c1r19s8.42tokyo.jp>
2022-05-03 21:15:14 +0900

    :recycle: Refactor gnl by combining static values to struct

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: susami <susami@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/28 11:21:27 by susami            #+#    #+#             */
-/*   Updated: 2022/05/03 19:23:23 by susami           ###   ########.fr       */
+/*   Updated: 2022/05/03 21:14:55 by susami           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,45 +16,46 @@
 # define BUFFER_SIZE 1024
 #endif
 
+typedef struct s_buf
+{
+	char	buf[BUFFER_SIZE + 1];
+	char	*cursor;
+	int		rc;
+	int		prev_fd;
+}	t_buf;
+
+static void	t_buf_init(t_buf *b, int fd)
+{
+	b->prev_fd = fd;
+	b->cursor = NULL;
+	b->rc = BUFFER_SIZE;
+}
+
 char	*get_next_line(int fd)
 {
-	static char	buf[BUFFER_SIZE + 1] = {0};
-	static int	prev_fd = -1;
-	static char	*cursor = NULL;
-	int			rc;
-	static int	read_next = 1;
-	char		*next_line;
+	static t_buf	b = {{0}, NULL, 0, -1};
+	char			*next_line;
 
 	next_line = NULL;
-	if (prev_fd != fd)
+	if (b.prev_fd != fd)
+		t_buf_init(&b, fd);
+	while (b.cursor || b.rc == BUFFER_SIZE)
 	{
-		prev_fd = fd;
-		cursor = NULL;
-		read_next = 1;
-	}
-	while (cursor || read_next)
-	{
-		if (cursor == NULL)
+		if (b.cursor == NULL)
 		{
-			rc = read(fd, buf, BUFFER_SIZE);
-			// 1. read fail
-			if (rc < 0)
+			b.rc = read(fd, b.buf, BUFFER_SIZE);
+			if (b.rc < 0)
 				return (NULL);
-			buf[rc] = '\0';
-			cursor = buf;
-			if (rc < BUFFER_SIZE)
-				read_next = 0;
+			b.buf[b.rc] = '\0';
+			b.cursor = b.buf;
 		}
-		while (*cursor)
+		while (*b.cursor)
 		{
-			// 1. append(next_line, *cursor)
-			next_line = append_realloc(next_line, *cursor);
-			// 2. if *cursor == '\n' return
-			if (*cursor++ == '\n')
+			next_line = append_realloc(next_line, *b.cursor);
+			if (*b.cursor++ == '\n')
 				return (next_line);
 		}
-		// 2. if cursor is '\0'
-		cursor = NULL;
+		b.cursor = NULL;
 	}
 	return (next_line);
 }
